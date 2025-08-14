7bbf637996579cf07d4fa5191cee7a1a8851cd89
ChiragParmar <chigsparmar07@gmail.com>
2025-08-14 21:08:58 +0530

    some changes

--- client/src/components/topics/Topics.js
+++ client/src/components/topics/Topics.js
@@ -1,34 +1,32 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import TopicList from './TopicList'
-import AddTopic from './AddTopic'
-import { getTopics } from '../../store/topic-slice'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import TopicList from './TopicList';
+import AddTopic from './AddTopic';
+import { getTopics } from '../../store/topic-slice';
 
 const Topics = () => {
-    const dispatch = useDispatch()
+	const dispatch = useDispatch();
 
-    const topics = useSelector(state => state.topic.topics)
-    const { isLoggedin } = useSelector(state => state.user)
+	const topics = useSelector((state) => state.topic.topics);
+	const { isLoggedin } = useSelector((state) => state.user);
 
-    const navigate = useNavigate()
-    useEffect(() => {
-        if (!localStorage.getItem("userData")) {
-            navigate('/')
-        }
-        dispatch(getTopics())
-    }, [dispatch, isLoggedin, navigate]);
+	const navigate = useNavigate();
+	useEffect(() => {
+		if (!localStorage.getItem('userData')) {
+			navigate('/');
+		}
+		dispatch(getTopics());
+	}, [dispatch, isLoggedin, navigate]);
 
-    return (
-        <>
-            <AddTopic />
-            {
-                topics.map(topic => {
-                    return <TopicList key={topic._id} topicName={topic.topicName} />
-                })
-            }
-        </>
-    )
-}
+	return (
+		<>
+			<AddTopic />
+			{topics.map((topic) => {
+				return <TopicList key={topic._id} topicName={topic.topicName} />;
+			})}
+		</>
+	);
+};
 
-export default Topics
+export default Topics;
